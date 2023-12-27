3a5530ad9a5653817cbc2dcb317f39ff1df334ba
fathimashaikit <114642809+fathimashaikit@users.noreply.github.com>
2023-12-27 12:27:08 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@
 class Simple{
 public static void main(String args[]){
 
-System.out.println("Hello Java hii chandra");
+System.out.println("Hello Java hii chandra and now i want to do some changes");
 
 }
 }
